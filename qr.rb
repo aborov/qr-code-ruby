22a12194aee706784a2ea9f13cbaafe47bdb0410
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2025-01-22 21:38:25 +0000

    implement the rest

--- qr.rb
+++ qr.rb
@@ -17,17 +17,45 @@ if input == "4"
 elsif input == "1"
   puts "Enter the URL that you'd like to encode:"
   url = gets.chomp
-  encode = url.to_s
+  encode = url
+
   puts "Enter the name for the PNG file:"
   name = gets.chomp
-  
+
+elsif input == "2"
+  puts "Enter the name of the wifi network:"
+  ssid = gets.chomp
+
+  puts "Enter the password:"
+  password = gets.chomp
+
+  puts "Enter the name for the PNG file:"
+  name = gets.chomp
+
+  encode = "WIFI:T:WPA;S:#{ssid};P:#{password};;"
+
+elsif input == "3"
+  puts "Enter the phone number you want the code to send a message to:"
+  phone = gets.chomp
+
+  puts "Enter the text for the message:"
+  message = gets.chomp
+
+  puts "Enter the name for the PNG file:"
+  name = gets.chomp
+
+  encode = "SMSTO:#{phone}:#{message}"
 end
 
-# Use the RQRCode::QRCode class to encode some text
-qrcode = RQRCode::QRCode.new(encode)
+if encode
+  # Generate the QR code
+  qrcode = RQRCode::QRCode.new(encode)
 
-# Use the .as_png method to create a 500 pixels by 500 pixels image
-png = qrcode.as_png({ :size => 500 })
+  # Convert the QR code to a PNG image
+  png = qrcode.as_png(size: 500)
 
-# Write the image data to a file
-IO.binwrite("#{name}.png", png.to_s)
+  # Write the QR code image to a file
+  IO.binwrite("#{name}.png", png.to_s)
+
+  puts "QR Code saved as #{name}.png!"
+end
